8db620fd6615ca41bcc3d5c553815ced84676d20
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-06 19:03:04 +0600

    addad the perameter

---
+++ parameter.js
@@ -0,0 +1,19 @@
+function vatKhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate khabar daw');
+}
+// vatKhao();
+
+function square (number){
+    const borgo = number * number;
+    console.log(borgo);
+}
+square(4);
+square(12);
+
+function add (a, b, c){
+    const d = a + b + c;
+    console.log(d);
+}
+add(5, 5, 5)
