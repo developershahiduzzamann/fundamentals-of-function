36d2c2730d7142ef2fbdfec6ef88bf9800b01f8a
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-07 06:53:19 +0600

    addad the recap

--- recap.js
+++ recap.js
@@ -2,4 +2,12 @@ function doubleIt (number){
     const dubled = number *2;
     console.log(number, dubled);
 }
-doubleIt(15);
+// doubleIt(15);
+
+function difference (n1, n2){
+    const diff = n1 - n2;
+    console.log(diff);
+}
+const fAge = 30;
+const mAge = 25;
+difference(fAge, mAge);
