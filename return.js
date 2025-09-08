fc4cee02f624c92407e0efef1a5c7b8bce928f55
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-08 10:48:13 +0600

    addad the new line

--- return.js
+++ return.js
@@ -5,3 +5,10 @@ function tenTimes (number){
 
 const output = tenTimes(5);
 console.log(output);
+
+function snumber(num){
+    const sresult = num / 2;
+    return sresult;
+}
+const half = snumber(1000)
+console.log(half);
