1057055e9243a17248cd079eac75a2cb35bd3daf
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-09 10:52:41 +0600

    add a short function

--- recapTwo.js
+++ recapTwo.js
@@ -4,3 +4,13 @@ function mod(p, q){
 }
 const stotal = mod(100, 50);
 console.log(stotal);
+
+
+// short
+
+function add (num1, num2){
+    return num1 + num2;
+}
+const addad = add(530, 530);
+console.log(addad);
+
