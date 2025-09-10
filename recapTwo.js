f6904ba1551e4e81c6f981a9263514a3c2fd617e
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-10 21:14:49 +0600

    add the return

--- recapTwo.js
+++ recapTwo.js
@@ -14,3 +14,14 @@ function add (num1, num2){
 const addad = add(530, 530);
 console.log(addad);
 
+
+
+function domath (num3, num4){
+    const sum = num3 + num4;
+    const diff = num3 - num4;
+    const mult = num3 * num4;
+    const result = mult /2;
+    return result
+}
+const result = domath(34, 45);
+console.log(result);
